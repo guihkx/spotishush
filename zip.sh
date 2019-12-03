79b20f2863e921b65019da97801fdf090bfb4c89
GUiHKX <GUiHKX@users.noreply.github.com>
2019-12-03 02:49:32 -0300

    Delete zip.sh
    Now we'll use node to make packaging easier

--- zip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-FILE='spotishush.zip'
-
-echo "Packing extension into ZIP file: ${PWD}/${FILE}"
-
-zip -FS "${FILE}" -r 'img/' '_locales/' 'manifest.json' 'spotishush.js' 'background.js'
